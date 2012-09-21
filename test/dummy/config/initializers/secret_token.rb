38216faec4f158f3f3f9213ac115aa3f69a23998
Sergei O. Udalov <sergei.udalov@gmail.com>
2012-09-21 15:46:58 +0400

    v0.1.0 Rails 3.2.x

--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '36ad349460da08994f46dcd7110115477a72797eff2d77f3f386ef37334d60fc7363b39c07a848966ae1f02fa10e5ecbf405c197d90f5aac2c8d46ac7fceb910'
+Dummy::Application.config.secret_token = '2618aadc9000a17309455fe2260bb5c06b5da89e330c088d10ac81f2e0e2cfa9098737959e2380fbdd1f18c7cd1f4c4cd384e8fbc59a99d6cade7ceddec16172'
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120806150342) do
+ActiveRecord::Schema.define(:version => 20120803095142) do
 
   create_table "liquid_cms_pages", :force => true do |t|
     t.string   "slug"
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "liquid_cms_pages", ["slug"], :name => "index_liquid_cms_pages_on_slug", :unique => true
@@ -26,8 +26,8 @@ ActiveRecord::Schema.define(:version => 20120806150342) do
   create_table "liquid_cms_text_blocks", :force => true do |t|
     t.string   "key"
     t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "liquid_cms_text_blocks", ["key"], :name => "index_liquid_cms_text_blocks_on_key", :unique => true
